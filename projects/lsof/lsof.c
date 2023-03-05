23d63f7d7fa7cc06854478163014dd7a6392fe2d
Mustafa Selçuk Çağlar <selcukcaglar08@gmail.com>
2023-03-05 10:05:47 +0300

    lsof getopt is done not tested!

--- projects/lsof/lsof.c
+++ projects/lsof/lsof.c
@@ -1,5 +1,5 @@
 /**
- * Copyright 2023 developer
+ * Copyright 2023 Mustafa Selçuk Çağlar(develooper1994)
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,3 +14,48 @@
  * limitations under the License.
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+int main(int argc, char* argv[]){
+    DIR* dir;
+    struct dirent* entry;
+    char path[64];
+    int fd;
+    int pid = -1;
+    char* command = NULL;
+
+    // Parse command line arguments
+    int opt;
+    while((opt=getopt(argc, argv, "p:c:")) != -1){
+        switch(opt){
+            case 'p':
+                pid = atoi(optarg);
+                break;
+            case 'c':
+                command = atoi(optarg);
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-p PID] [-c command]\n", argv[0]);
+                return EXIT_FAILURE;
+        }
+    }
+
+    if(pid != -1){
+
+    }
+    else if(command != NULL){
+
+    }
+    else{
+
+    }
+
+    return EXIT_SUCCESS;
+}
--- projects/lsof/lsof.md
+++ projects/lsof/lsof.md
@@ -1,5 +1,5 @@
 <!--
- Copyright 2023 developer
+ Copyright 2023 Mustafa Selçuk Çağlar(develooper1994)
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
